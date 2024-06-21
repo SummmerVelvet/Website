b9d074db8f0f0f054a94d808f14b8740c694582d
Yogya Gehlot <summervelvet582@gmail.com>
2024-06-21 16:15:11 +0530

    Slide shit added

--- smt.js
+++ smt.js
@@ -1,8 +1,8 @@
 const left = document.getElementById("left-side");
 
 const handleOnMove = e => {
-    const p = e.clintX / window.innerWidth * 100;
-    left.style.width = '${p}%'
+    const p = e.clientX / window.innerWidth * 100;
+    left.style.width = `${p}%`;
 }
 
 document.onmousemove = e => handleOnMove(e);
